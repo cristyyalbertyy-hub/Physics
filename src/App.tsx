79036f979658a95acb2d1a133b34e1ae5da21162
Cristy Alberty <cristyyalbertyy@gmail.com>
2026-05-19 00:39:34 +0100

    mensagem

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ export default function App() {
 
   const selectSubchapter = (sel: Selection) => {
     setSelection(sel);
-    setOpenGroups(collapsedGroups());
   };
 
   const goToEntry = () => {
